28763a22545ed94c03ba5c4284a2e6cd6bb03af2
Gavin R. Miller <grmiller@convergeone.com>
2018-06-01 16:04:59 -0500

    Replaced Bower w/ NPM/Webpack combo

---
+++ assets.js
@@ -0,0 +1,21 @@
+//const CSS = [
+//    'jquery-ui-dist/jquery-ui.min.css',
+//    'jquery-datetimepicker/jquery.datetimepicker.css'
+//];
+//const JS = [
+//    'jquery-ui-dist/jquery-ui.min.js',
+//    'jquery-datetimepicker/jquery.datetimepicker.min.js',
+//    'jquery/dist/jquery.min.js',
+//    'lodash/lodash.min.js'
+//];
+
+//Whole package folder paths, this replaces bower
+const JS = [
+    'angular',
+    'angular-bootstrap',
+    'angular-mocks',
+    'angular-ui-router',
+    'bootstrap'
+];
+
+module.exports = [...JS];
